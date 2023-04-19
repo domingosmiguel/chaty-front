ce4b3ffb5f7ca44844d207811ed79b1384f46562
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2023-04-18 23:32:20 -0300

    chore: adjust docker configs

--- services/api.tsx
+++ services/api.tsx
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-console.log(
-  '🚀 ~ file: api.tsx:6 ~ process.env.NEXT_PUBLIC_API_BASE_URL:',
-  process.env.NEXT_PUBLIC_API_BASE_URL
-);
 const instance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
 });
