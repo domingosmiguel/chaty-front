d5b22e5693c44aef20b5c05893423ff57521ad78
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2023-04-19 17:04:43 -0300

    Update api.tsx

--- services/api.tsx
+++ services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 console.log('🙂 process.env.NEXT_PUBLIC_API_BASE_URL: ', process.env.NEXT_PUBLIC_API_BASE_URL)
 const instance = axios.create({
-  baseURL: '${process.env.NEXT_PUBLIC_API_BASE_URL || ''}/api/',
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL || ''}/api/`,
 });
 console.log('🙂 instance: ', instance)
 export default instance;
