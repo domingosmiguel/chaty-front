8c8257aa88c76db718640d6183a06bba6726b371
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2023-05-31 15:13:04 -0300

    chore: adapt for vercel deploy

--- services/api.tsx
+++ services/api.tsx
@@ -4,6 +4,6 @@ console.log(
   process.env.NEXT_PUBLIC_API_BASE_URL
 );
 const instance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL || ''}/api/`,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || '/api/',
 });
 export default instance;
